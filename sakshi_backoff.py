fa2b212794ebf7640bba2bf1b97b026feef2e790
Sakshishettyp <sakshi@tetrisinnovations.com>
2025-12-24 21:59:12 +0530

    Implemented proper backoff function to improve error handling and retry logic

--- sakshi_backoff.py
+++ sakshi_backoff.py
@@ -4,29 +4,24 @@ import requests
 
 
 def backoff(url, max_retries, base_delay, jitter, timeout):
-    try:
-        requests.get(url, timeout=timeout)
-    except:
-        return
     for i in range(max_retries):
         try:
-            print(f"try {i+1}")
             r = requests.get(url, timeout=timeout)
             if r.status_code == 200:
+                print("Success")
                 return r.text
-        except:
-            pass
+        except Exception as e:
+            wait = base_delay * (2**i)
+            wait += random.uniform(-jitter, jitter)
 
-        wait = base_delay * (2**i)
-        wait += random.uniform(-jitter, jitter)
-
-        if wait < 0:
-            wait = 0
-
-        print(f"waiting {wait:.2f}s")
-        time.sleep(wait)
+            if wait < 0:
+                wait = 0
+            print(f"Try {i + 1}")
+            print(f"waiting {wait:.2f}s")
+            time.sleep(wait)
 
 
 if __name__ == "__main__":
-    backoff("https://httpstat.us/503", 5, 1.0, 0.25, 3.0)
+    backoff("aa", 3, 1.0, 0.25, 3.0)
 # https://httpstat.us/503
+# https://google.com
